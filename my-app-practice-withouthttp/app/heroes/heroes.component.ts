6731f74561206c063e69b1ce5e67edae86174828
nannan.c.wang@accenture.com <nKp6yRj7>
2018-03-07 15:29:15 +0800

    add angular practice without http

---
+++ my-app-practice-withouthttp/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+	  this.getHeroes();
+  }
+
+  hero: Hero = {
+	id: 1,
+	name: 'Windstorm'
+  };
+
+  //heroes = HEROES;
+  heroes: Hero[];
+  
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+  
+}
