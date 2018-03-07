e219448364598e1a0dbae7153f7d9d9a6e00f8fd
nannan.c.wang@accenture.com <nKp6yRj7>
2018-03-07 15:57:26 +0800

    http

---
+++ my-app-practice-withhttp-webapi/app/hero.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heros';
+import { MessageService } from './message.service';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class HeroService {
+
+  constructor(
+  private http: HttpClient,
+  private messageService: MessageService) { }
+  
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+  
+getHeroes(): Observable<Hero[]> {
+  // Todo: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
+  //return of(HEROES);
+  return this.http.get<Hero[]>(this.heroesUrl)
+}
+
+getHero(id: number): Observable<Hero> {
+	const url = `${this.heroesUrl}/${id}`;
+  // Todo: send the message _after_ fetching the hero
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero => hero.id === id));
+}
+}
